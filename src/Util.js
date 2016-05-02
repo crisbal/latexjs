37244e1b5faf727035c96beb11dcfdd482cda5e6
Cristian Baldi <bld.cris.96@gmail.com>
2016-05-02 16:05:59 +0200

    Add .escaped property

--- src/Util.js
+++ src/Util.js
@@ -1,7 +1,22 @@
 import util from "util";
+import lescape from "escape-latex";
 
 String.prototype.format = function(){
   var args = Array.prototype.slice.call(arguments);
   args.unshift(this.valueOf());
   return util.format.apply(util, args);
 };
+
+var addStringProperty = function (name, func) {
+    String.prototype.__defineGetter__(name, func);
+};
+
+
+var addArrayProperty = function (name, func) {
+    String.prototype.__defineGetter__(name, func);
+};
+
+
+addStringProperty('escaped', function () {
+    return lescape(this);
+});
